405e5e32f14bca3714760786d36fcbd9d2baf8fa
rburgessfl <rburgessfl@gmail.com>
2021-02-14 20:33:11 -0500

    added CORS policy

--- src/main/java/net/profitposition/api/Application.java
+++ src/main/java/net/profitposition/api/Application.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class Application {
@@ -15,4 +17,13 @@ public class Application {
     @Bean
     public RestTemplate restTemplate() {return new RestTemplate();}
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
+    }
 }
