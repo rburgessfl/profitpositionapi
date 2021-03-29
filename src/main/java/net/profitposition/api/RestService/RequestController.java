60567edf5f2ab0857b378d69e7bde5d0ef15ef9d
rburgessfl <rburgessfl@gmail.com>
2021-03-29 10:56:30 -0400

    Redefined APIs

--- src/main/java/net/profitposition/api/RestService/RequestController.java
+++ src/main/java/net/profitposition/api/RestService/RequestController.java
@@ -13,11 +13,6 @@ public class RequestController {
     @Autowired
     RequestHandler service;
 
-    @GetMapping("/datapackage/{symbol}")
-    public String execute0(@PathVariable String symbol) throws IOException {
-        return service.getDataPackage(symbol);
-    }
-
     @GetMapping("/atr/{symbol}")
     public String execute1(@PathVariable String symbol) throws IOException {
         return service.getAtr(symbol);
@@ -28,49 +23,9 @@ public class RequestController {
         return service.getBbands(symbol);
     }
 
-    @GetMapping("/dcf/{symbol}")
-    public String execute3(@PathVariable String symbol) throws IOException {
-        return service.getDcf(symbol);
-    }
-
     @GetMapping("/companyprofile/{symbol}")
     public String execute4(@PathVariable String symbol) throws IOException {
         return service.getCompanyProfile(symbol);
-    }
-
-    @GetMapping("/financialratios/{symbol}")
-    public String execute5(@PathVariable String symbol) throws IOException {
-        return service.getFinancialRatios(symbol);
-    }
-
-    @GetMapping("/incomestatement/{symbol}")
-    public String execute6(@PathVariable String symbol) throws IOException {
-        return service.getIncomeStatement(symbol);
-    }
-
-    @GetMapping("/balancesheetstatement/{symbol}")
-    public String execute7(@PathVariable String symbol) throws IOException {
-        return service.getBalanceSheetStatement(symbol);
-    }
-
-    @GetMapping("/cashflowstatement/{symbol}")
-    public String execute8(@PathVariable String symbol) throws IOException {
-        return service.getCashFlowStatement(symbol);
-    }
-
-    @GetMapping("/incomestatementgrowth/{symbol}")
-    public String execute9(@PathVariable String symbol) throws IOException {
-        return service.getIncomeStatementGrowth(symbol);
-    }
-
-    @GetMapping("/balancesheetstatementgrowth/{symbol}")
-    public String execute10(@PathVariable String symbol) throws IOException {
-        return service.getBalanceSheetStatementGrowth(symbol);
-    }
-
-    @GetMapping("/cashflowstatementgrowth/{symbol}")
-    public String execute11(@PathVariable String symbol) throws IOException {
-        return service.getCashFlowStatementGrowth(symbol);
     }
 
 }
--- src/main/java/net/profitposition/api/RestService/RequestHandler.java
+++ src/main/java/net/profitposition/api/RestService/RequestHandler.java
@@ -23,34 +23,8 @@ public class RequestHandler {
     public String bbands;
     public String bbandsData;
     public String bbandsValues;
-    public String dcf;
-    public String dcfValue;
     public String companyProfile;
     public String companyProfileValue;
-    public String financialRatios;
-    public String financialRatiosValues;
-    public String incomeStatements;
-    public String balanceSheetStatements;
-    public String cashFlowStatements;
-    public String incomeStatementsGrowth;
-    public String balanceSheetStatementsGrowth;
-    public String cashFlowStatementsGrowth;
-
-    public String getDataPackage(String symbol) throws IOException{
-        String a = getCompanyProfile(symbol);
-        String b = getDcf(symbol);
-        String c = getBbands(symbol);
-        String d = getAtr(symbol);
-        String e = getFinancialRatios(symbol);
-        String f = getIncomeStatement(symbol);
-        String g = getBalanceSheetStatement(symbol);
-        String h = getCashFlowStatement(symbol);
-        String i = getIncomeStatementGrowth(symbol);
-        String j = getBalanceSheetStatementGrowth(symbol);
-        String k = getCashFlowStatementGrowth(symbol);
-
-        return "["+a+","+b+","+c+","+d+","+e+","+f+","+g+","+h+","+i+","+j+","+k+"]";
-    }
 
     public String getAtr(String symbol) throws IOException{
         ResponseEntity<String> response = restTemplate.getForEntity(
@@ -78,17 +52,6 @@ public class RequestHandler {
         return bbandsValues;
     }
 
-    public String getDcf(String symbol) throws IOException{
-        ResponseEntity<String> response = restTemplate.getForEntity(
-                ("https://financialmodelingprep.com/api/v3/discounted-cash-flow/"+symbol+"?apikey="+apiKey2), String.class);
-        dcf = response.getBody();
-        ObjectMapper mapper = new ObjectMapper();
-        ObjectNode[] node = mapper.readValue(dcf, ObjectNode[].class);
-        System.out.println("Symbol: "+symbol+" - Discounted Cash Flow");
-        dcfValue = String.valueOf(node[0]);
-        return dcfValue;
-    }
-
     public String getCompanyProfile(String symbol) throws IOException{
         ResponseEntity<String> response = restTemplate.getForEntity(
                 ("https://financialmodelingprep.com/api/v3/profile/"+symbol+"?apikey="+apiKey2), String.class);
@@ -98,64 +61,6 @@ public class RequestHandler {
         System.out.println("Symbol: "+symbol+" - Company Profile");
         companyProfileValue = String.valueOf(node[0]);
         return companyProfileValue;
-    }
-
-    public String getFinancialRatios(String symbol) throws IOException{
-        ResponseEntity<String> response = restTemplate.getForEntity(
-                ("https://financialmodelingprep.com/api/v3/ratios-ttm/"+symbol+"?apikey="+apiKey2), String.class);
-        financialRatios = response.getBody();
-        ObjectMapper mapper = new ObjectMapper();
-        ObjectNode[] node = mapper.readValue(financialRatios, ObjectNode[].class);
-        System.out.println("Symbol: "+symbol+" - Financial Ratios");
-        financialRatiosValues = String.valueOf(node[0]);
-        return financialRatiosValues;
-    }
-
-    public String getIncomeStatement(String symbol) throws IOException{
-        ResponseEntity<String> response = restTemplate.getForEntity(
-                ("https://financialmodelingprep.com/api/v3/income-statement/"+symbol+"?period=annual&limit=5&apikey="+apiKey2), String.class);
-        incomeStatements = response.getBody();
-        System.out.println("Symbol: "+symbol+" - Income Statement");
-        return incomeStatements;
-    }
-
-    public String getBalanceSheetStatement(String symbol) throws IOException{
-        ResponseEntity<String> response = restTemplate.getForEntity(
-                ("https://financialmodelingprep.com/api/v3/balance-sheet-statement/"+symbol+"?period=annual&limit=5&apikey="+apiKey2), String.class);
-        balanceSheetStatements = response.getBody();
-        System.out.println("Symbol: "+symbol+" - Balance Sheet Statements");
-        return balanceSheetStatements;
-    }
-
-    public String getCashFlowStatement(String symbol) throws IOException{
-        ResponseEntity<String> response = restTemplate.getForEntity(
-                ("https://financialmodelingprep.com/api/v3/cash-flow-statement/"+symbol+"?period=annual&limit=5&apikey="+apiKey2), String.class);
-        cashFlowStatements = response.getBody();
-        System.out.println("Symbol: "+symbol+" - Cash Flow Statements");
-        return cashFlowStatements;
-    }
-    public String getIncomeStatementGrowth(String symbol) throws IOException{
-        ResponseEntity<String> response = restTemplate.getForEntity(
-                ("https://financialmodelingprep.com/api/v3/income-statement-growth/"+symbol+"?period=annual&limit=5&apikey="+apiKey2), String.class);
-        incomeStatementsGrowth = response.getBody();
-        System.out.println("Symbol: "+symbol+" - Income Statement Growth");
-        return incomeStatementsGrowth;
-    }
-
-    public String getBalanceSheetStatementGrowth(String symbol) throws IOException{
-        ResponseEntity<String> response = restTemplate.getForEntity(
-                ("https://financialmodelingprep.com/api/v3/balance-sheet-statement-growth/"+symbol+"?period=annual&limit=5&apikey="+apiKey2), String.class);
-        balanceSheetStatementsGrowth = response.getBody();
-        System.out.println("Symbol: "+symbol+" - Balance Sheet Statements Growth");
-        return balanceSheetStatementsGrowth;
-    }
-
-    public String getCashFlowStatementGrowth(String symbol) throws IOException{
-        ResponseEntity<String> response = restTemplate.getForEntity(
-                ("https://financialmodelingprep.com/api/v3/cash-flow-statement-growth/"+symbol+"?period=annual&limit=5&apikey="+apiKey2), String.class);
-        cashFlowStatementsGrowth = response.getBody();
-        System.out.println("Symbol: "+symbol+" - Cash Flow Statements Growth");
-        return cashFlowStatementsGrowth;
     }
 
 }
